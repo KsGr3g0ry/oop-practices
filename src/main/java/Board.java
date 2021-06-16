fdc2f78c9ab7c391b1bc7718014b5a18099297d2
KsGr3g0ry <gbrothers29@gmail.com>
2021-06-16 17:22:08 -0500

    numbers 1-9 added to be displayed on board when game starts

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,7 @@
 public class Board {
+    public static void main(String[] args) {
+        System.out.println("1 | 2 | 3");
+        System.out.println("4 | 5 | 6");
+        System.out.println("7 | 8 | 9");
+    }
 }
